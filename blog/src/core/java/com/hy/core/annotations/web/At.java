b1023ffda9bf86339c24a1640deb36aa9b616be1
huangyi8973 <huangyi8973@163.com>
2014-05-26 21:49:20 +0800

    可以没有url参数，如果在类名上有指定了url范围，比如/login，方法上只要加上@At就变成默认调用了

--- blog/src/core/java/com/hy/core/annotations/web/At.java
+++ blog/src/core/java/com/hy/core/annotations/web/At.java
@@ -10,6 +10,6 @@ import com.hy.core.pub.HttpMethod;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD,ElementType.TYPE})
 public @interface At {
-	String value();
+	String value() default "";
 	HttpMethod method() default HttpMethod.GET;
 }
